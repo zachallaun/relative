acc336e5f45126154a76863839f4f3a8c4d08707
Zachary Allaun <zach.allaun@gmail.com>
2012-08-06 11:55:06 -0400

    Make variable names not super shitty

--- src/relative/trueskill.clj
+++ src/relative/trueskill.clj
@@ -11,23 +11,23 @@
 ;;
 
 (defn variance
-  "The variance of a standard deviation sigma."
-  [sigma] (Math/pow sigma 2))
+  "The variance of a standard deviation stdd."
+  [stdd] (Math/pow stdd 2))
 
 (defn precision
   "The precision of a standard deviation, which is the inverse of the variance."
-  [sigma]
-  (/ 1 (variance sigma)))
+  [stdd]
+  (/ 1 (variance stdd)))
 
 (defn pdf
   "Normal probability density function:
 
   The value of a normal distribution at a given point x, with the distribution
-  described by mean mu and standard deviation sigma."
-  [x mu sigma]
-  (let [exp (- (* (/ 1 (* 2 (variance sigma)))
-                  (Math/pow (- x mu) 2)))]
-    (* (/ 1 (* sigma (Math/sqrt (* 2 Math/PI))))
+  described by mean mean and standard deviation stdd."
+  [x mean stdd]
+  (let [exp (- (* (/ 1 (* 2 (variance stdd)))
+                  (Math/pow (- x mean) 2)))]
+    (* (/ 1 (* stdd (Math/sqrt (* 2 Math/PI))))
        (Math/pow Math/E exp))))
 
 (defn error-fn
@@ -44,63 +44,63 @@
             (Math/pow Math/E (- (Math/pow x 2)))))))
 
 (defn cdf
-  [x mu sigma]
-  (let [xp (/ (- x mu) (* sigma (Math/sqrt 2)))]
+  [x mean stdd]
+  (let [xp (/ (- x mean) (* stdd (Math/sqrt 2)))]
     (* 1/2 (+ 1 (if (> x 0)
                   (error-fn xp)
                   (- (error-fn (- xp))))))))
 
-;; (defn cumulative-dist
-;;   "Approximation of the cumulative distribution function when -1 < t < 1
+;; (defn cumeanlative-dist
+;;   "Approximation of the cumeanlative distribution function when -1 < t < 1
 ;;   given a normal distribution with the given precision."
 ;;   [t]
 ;;   (+ 1/2 (/ t (Math/sqrt (* 2 (Math/PI))))))
 
 (defprotocol ITrueSkillPlayer
-  (mu [_]
+  (mean [_]
     "Average expected skill/rating of a player.")
 
-  (sigma [_]
+  (stdd [_]
     "Standard deviation in skill of a player")
 
   (v [_ t e]
     "Mean Additive Truncated Gaussian Function."))
 
-(defrecord TrueSkillPlayer [id mu-atom sigma-atom]
+(defrecord TrueSkillPlayer [id mean-atom stdd-atom]
   ITrueSkillPlayer
-  (mu [_] @mu-atom)
+  (mean [_] @mean-atom)
 
-  (sigma [_] @sigma-atom)
+  (stdd [_] @stdd-atom)
 
   (v [_ t e]
-    (/ (pdf (- t e) @mu-atom @sigma-atom)
-       (cdf (- t e) @mu-atom @sigma-atom)))
+    (/ (pdf (- t e) @mean-atom @stdd-atom)
+       (cdf (- t e) @mean-atom @stdd-atom)))
 
   IRelativeRatedPlayer
-  (rating [_] (- @mu-atom (* 3 @sigma-atom))))
+  (rating [_] (- @mean-atom (* 3 @stdd-atom))))
 
 (defprotocol ITrueSkillEngine
   (beta-sq [_]
     "Variance of performance around the skill of each player. Determined
-    by the initial sigma spread.")
+    by the initial stdd spread.")
 
   (tau-sq [_]
-    "Additive dynamics factor. Determined by the initial sigma spread.")
+    "Additive dynamics factor. Determined by the initial stdd spread.")
 
   (c-value [_ s1 s2]
     "Normalizing value. Determined by the performance variance of the
-    system (beta-sq), and the sigma spread of both players going into a match.")
+    system (beta-sq), and the stdd spread of both players going into a match.")
 
-  (mu-additive-factors [_ winner loser]
-    "Returns a pair of mu additive factors corresponding to the winner and loser."))
+  (mean-additive-factors [_ winner loser]
+    "Returns a pair of mean additive factors corresponding to the winner and loser."))
 
-(deftype TrueSkillEngine [init-sigma init-mu draw]
+(deftype TrueSkillEngine [init-stdd init-mean draw]
   ITrueSkillEngine
   (beta-sq [_]
-    (Math/pow (/ init-sigma 2) 2))
+    (Math/pow (/ init-stdd 2) 2))
 
   (tau-sq [_]
-    (Math/pow (/ init-sigma 100) 2))
+    (Math/pow (/ init-stdd 100) 2))
 
   (c-value [this s1 s2]
     (Math/sqrt (+ (* 2 (beta-sq this))
@@ -108,25 +108,27 @@
                   (variance s2))))
 
   ;; Clearly broken
-  (mu-additive-factors [this winner loser]
-    (let [c (c-value this (sigma winner) (sigma loser))
-          t (/ (- (mu winner) (mu loser)) c)
+  (mean-additive-factors [this winner loser]
+    (let [c (c-value this (stdd winner) (stdd loser))
+          t (/ (- (mean winner) (mean loser)) c)
           e (/ draw c)
           m-winner (v winner t e)
           m-loser (v loser t e)
           normalized-variance (fn [s]
                              (/ (variance s) c))]
-      [(* (normalized-variance (sigma winner)) m-winner)
-       (* (normalized-variance (sigma loser)) m-loser)]))
+      [(* (normalized-variance (stdd winner)) m-winner)
+       (* (normalized-variance (stdd loser)) m-loser)]))
 
   IRelativeRatingEngine
   (map->player [_ {:keys [id seed opts]}]
-    (->TrueSkillPlayer id (atom (or seed init-mu)) (atom (or (:sigma opts) init-sigma)))))
+    (->TrueSkillPlayer id
+                       (atom (or seed init-mean))
+                       (atom (or (:stdd opts) init-stdd)))))
 
 (defn trueskill-engine
-  "Accepts optional keyword arguments to specify an initial sigma spread,
-  mu skill value, and draw margin.
+  "Accepts optional keyword arguments to specify an initial stdd,
+  mean skill value, and draw margin.
 
-  Defaults to (trueskill-engine :sigma 25/3 :mu 25 :draw 0)"
-  [& {:keys [sigma mu draw]}]
-  (->TrueSkillEngine (or sigma 25/3) (or mu 25) (or draw 4)))
+  Defaults to (trueskill-engine :stdd 25/3 :mean 25 :draw 0)"
+  [& {:keys [stdd mean draw]}]
+  (->TrueSkillEngine (or stdd 25/3) (or meant 25) (or draw 4)))
